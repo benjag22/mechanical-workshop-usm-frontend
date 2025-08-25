8aaaa049ab75be080c3835e3677ff1793045cc60
benjag22 <benjagonzalez2022@udec.cl>
2025-08-24 22:45:19 -0400

    feat: add main to children section and add expanded state in root layout to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from "next";
+'use client'
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/app/components/NavBar";
+import React, {useState} from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,23 +15,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Records management USM",
-  description: "Mechanical Workshop by USM",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const [isExpanded, setIsExpanded] = useState(false);
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        {children}
+        <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
+        <main
+            className={`
+            min-h-screen
+            transition-all
+            duration-300
+            pt-4
+            px-4
+            md:pt-6
+            md:px-6
+            ${isExpanded ? 'md:ml-[280px]' : 'md:ml-[80px]'}
+          `}
+        >
+            {children}
+        </main>
       </body>
     </html>
   );
