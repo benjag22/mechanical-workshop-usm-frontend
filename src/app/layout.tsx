dd481aa79c9a4d15f29b042f399e4c0bb588d1b5
benjag22 <benjagonzalez2022@udec.cl>
2025-08-26 23:20:58 -0400

    !feat: remove unnecessary transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
 'use client'
 
-import { Geist, Geist_Mono } from "next/font/google";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import NavBar from "@/app/components/NavBar";
 import React, {useState} from "react";
 import {cn} from "@/app/cn";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
     const [isExpanded, setIsExpanded] = useState(false);
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiasing`}
-      >
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiasing`}
+        >
         <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
         <main
-            className={cn("min-h-screen transition-all duration-300", isExpanded ? 'ml-[280px]' : 'ml-[80px]')}
+            className={cn("min-h-screen", isExpanded ? 'ml-[280px]' : 'ml-[80px]')}
         >
             {children}
         </main>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
