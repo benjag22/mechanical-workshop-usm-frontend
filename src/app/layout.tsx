4151ac654086c65e6d5ef40580194fc46accde38
benjag22 <benjagonzalez2022@udec.cl>
2025-09-08 17:05:42 -0300

    refactor: layout was changed to a grid calculate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,17 +22,23 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     const [isExpanded, setIsExpanded] = useState(false);
+
     return (
         <html lang="en" suppressHydrationWarning>
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiasing`}
         >
-        <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
-        <main
-            className={cn("min-h-screen", isExpanded ? 'ml-[240px] lg:ml-[280px]' : 'ml-[70px] lg:ml-[80px]')}
-        >
-            {children}
-        </main>
+        <div className={cn(
+            "min-h-screen grid transition-all duration-300",
+            isExpanded
+                ? "grid-cols-[240px_1fr] lg:grid-cols-[280px_1fr]"
+                : "grid-cols-[70px_1fr] lg:grid-cols-[80px_1fr]"
+        )}>
+            <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
+            <main className={cn("min-h-screen overflow-x-hidden")}>
+                {children}
+            </main>
+        </div>
         </body>
         </html>
     );
