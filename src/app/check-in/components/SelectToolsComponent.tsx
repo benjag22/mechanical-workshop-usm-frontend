e2f7d35323773c9ed1ab6da553c3d022399941e1
benjag22 <benjagonzalez2022@udec.cl>
2025-09-09 18:13:17 -0300

    styles: change grid cols columns on tools select

--- src/app/check-in/components/SelectToolsComponent.tsx
+++ src/app/check-in/components/SelectToolsComponent.tsx
@@ -66,7 +66,7 @@ export default function SelectToolsComponent() {
                     Selecciona las herramientas disponibles en el vehículo
                 </p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-6">
+            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 mb-6">
                 {options.map((tool) => {
                     const selected = isToolSelected(tool);
                     return (
